d674a51b23ba28275189dfde4f518a41ebee8f3a
Mslef <marcel.lefebvre01@gmail.com>
2015-10-26 16:12:30 -0400

    feat: update config

--- config.sh
+++ config.sh
@@ -9,6 +9,12 @@ apt-get -qqy install php5-curl
 apt-get -qqy install npm
 apt-get -qqy install pylint
 apt-get -qqy install php-pear
+apt-get -qqy install graphicsmagick
+apt-get -qqy install ruby
+apt-get -qqy install ruby-sass
+apt-get -qqy install php5-mcrypt
+apt-get -qqy install php5-curl
+apt-get -qqy install python-mysqldb
 pip install oauth2client
 pip install django djangorestframework
 pip install requests
@@ -16,6 +22,9 @@ pip install httplib2
 pip install pep8
 pip install selenium
 npm install -g jshint
+npm install -g grunt-cli
+npm install -g bower
+npm install -g yo
 pear install PHP_CodeSniffer
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
